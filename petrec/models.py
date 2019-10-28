d25f12d6b68a3f98ac413672b97c39ed08594328
Thiago Alexandre Fernandes Silvs <thiagoynos@gmail.com>
2019-10-28 20:04:15 -0300

    Criação das Moldels

--- petrec/models.py
+++ petrec/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class TB_Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11)
+    email = models.CharField(max_length=60)
+    senha = models.CharField(max_length=100)
+
+
+
+class TB_Cachorro(models.Model):
+    nome_c = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=500)
+    raca = models.CharField(max_length=100)
+    local = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=1)
+    recompensa = models.CharField(max_length=10)
