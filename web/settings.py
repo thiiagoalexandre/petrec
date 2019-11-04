727bc610baee2e489ea80514342a613bd9f1efe8
UniCEUB <thiago.asilva@sempreceub.com>
2019-11-04 06:14:40 -0300

    Adiciona dependências

--- web/settings.py
+++ web/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'pt-BR'
+#LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'UTC'
 
