22f5b827a4d201992aac6f8201abc37c152b3738
UniCEUB <thiago.asilva@sempreceub.com>
2019-11-04 06:59:50 -0300

    Inserindo configurações para app executar no heroku

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 install_requires = [
     'django',
+    'gunicorn',
+    'django-heroku'
     ]
 
 
--- web/settings.py
+++ web/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -113,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
@@ -120,3 +125,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "{}/media".format(BASE_DIR)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
