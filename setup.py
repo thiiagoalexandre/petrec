3711ad6b4f5e4caf6429a13e00c5af923dfe27d4
Thiago Alexandre Fernandes Silvs <thiagosilva@Thiagos-MacBook-Air-2.local>
2019-11-06 19:45:59 -0300

    resolvendo problema para deploy no mac

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 install_requires = [
     'django',
     'gunicorn',
-    'django-heroku'
+    'django-heroku',
     ]
 
 
